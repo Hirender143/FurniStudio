b11ecc05128e8d7186bd1ebc4e5d7a8dc13517ad
Hirender143 <hirenderp@gmail.com>
2024-10-11 12:07:59 +0530

    change collection cart file from Set TO List

--- src/main/java/Com/Furni/entity/Cart.java
+++ src/main/java/Com/Furni/entity/Cart.java
@@ -1,52 +1,57 @@
 package Com.Furni.entity;
 
-import jakarta.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "cart")
 public class Cart {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 
-    @ManyToMany
-    @JoinTable(
-        name = "cart_product", 
-        joinColumns = @JoinColumn(name = "cart_id"),
-        inverseJoinColumns = @JoinColumn(name = "product_id")
-    )
-    private Set<Product> products = new HashSet<>();
-    
-    @OneToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+	@ManyToMany
+	@JoinTable(name = "cart_product", joinColumns = @JoinColumn(name = "cart_id"), inverseJoinColumns = @JoinColumn(name = "product_id"))
+	private List<Product> products = new ArrayList<>();
 
-    // Add methods to add/remove products
-    public void addProduct(Product product) {
-        products.add(product);
-    }
+	@OneToOne
+	@JoinColumn(name = "user_id")
+	private User user;
 
-    public void removeProduct(Product product) {
-        products.remove(product);
-    }
+	// Add methods to add/remove products
+	public void addProduct(Product product) {
+		products.add(product);
+	}
 
-    public Set<Product> getProducts() {
-        return products;
-    }
-    
-    public Long getId() {
-        return id;
-    }
+	public void removeProduct(Product product) {
+		products.remove(product);
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public List<Product> getProducts() {
+		return products;
+	}
 
-	public void setProducts(Set<Product> products) {
+	public void setProducts(List<Product> products) {
 		this.products = products;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public User getUser() {
@@ -56,8 +61,5 @@ public class Cart {
 	public void setUser(User user) {
 		this.user = user;
 	}
-	
-	
-    
-    
+
 }
